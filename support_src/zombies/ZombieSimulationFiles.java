0a7d9ab57153db1d70a4cb05ca562d3b986e666b
Dennis Cosgrove <cosgroved@wustl.edu>
2021-03-14 22:03:18 -0500

    url -> uri -> file to account for spaces in directory names

--- support_src/zombies/ZombieSimulationFiles.java
+++ support_src/zombies/ZombieSimulationFiles.java
@@ -2,6 +2,8 @@ package zombies;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
 
 import support.cse131.ArgsProcessor;
@@ -12,10 +14,18 @@ import support.cse131.Scanners;
  * @author Dennis Cosgrove (http://www.cse.wustl.edu/~cosgroved/)
  */
 public class ZombieSimulationFiles {
+	private static File toFile(URL url) {
+		try {
+			URI uri = url.toURI();
+			return new File(uri);
+		} catch (URISyntaxException urise) {
+			return null;
+		}
+	}
+
 	public static ArgsProcessor createArgsProcessorFromFile(String[] argsFromMain) {
 		URL url = ZombieSimulationFiles.class.getResource("resources");
-		String directoryName = url.getFile();
-		File directory = new File(directoryName);
+		File directory = toFile(url);
 		File file = FileChoosers.chooseFile(argsFromMain, directory);
 		if (file != null && file.exists()) {
 			try {
