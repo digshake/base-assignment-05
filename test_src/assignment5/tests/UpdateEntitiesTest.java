2ffaf283f024b183c14227446aa31634262e1f51
joshpullen <jdpullen@wustl.edu>
2021-10-27 11:21:11 -0500

    Updated Entities Tests to cover more incorrect cases

--- test_src/assignment5/tests/UpdateEntitiesTest.java
+++ test_src/assignment5/tests/UpdateEntitiesTest.java
@@ -402,4 +402,173 @@ public class UpdateEntitiesTest {
 		}
 		assertEquals(positions.length, zombieCount);
 	}
+
+	/**
+	 * Verify that no entity moves by more than RANDOM_DELTA_HALF_RANGE, including after touching the boundary.
+	 * The individual testSingleNonZombieMotion and testSingleZombieMotion test make sure that they don't move
+	 * farther than (-delta, delta), but only for standard movement - not on the boundary.
+	 */
+	@Test
+	public void testMaxMoveDistance() {
+		double[][] positions = {
+				{ 0.0001, .9999 },
+				{ 0.0001, .0001 },
+				{ 0.9999, .0001 },
+				{ 0.9999, .9999 },
+				{ 0.5, .9999 },
+				{ 0.5, .0001 },
+				{ 0.9999, 0.5 },
+				{ 0.9999, 0.5 },
+				{ 0.0001, .9999 },
+				{ 0.0001, .0001 },
+				{ 0.9999, .0001 },
+				{ 0.9999, .9999 },
+				{ 0.5, .9999 },
+				{ 0.5, .0001 },
+				{ 0.9999, 0.5 },
+				{ 0.9999, 0.5 },
+				{ 0.0001, .9999 },
+				{ 0.0001, .0001 },
+				{ 0.9999, .0001 },
+				{ 0.9999, .9999 },
+				{ 0.5, .9999 },
+				{ 0.5, .0001 },
+				{ 0.9999, 0.5 },
+				{ 0.9999, 0.5 },
+				{ 0.0001, .9999 },
+				{ 0.0001, .0001 },
+				{ 0.9999, .0001 },
+				{ 0.9999, .9999 },
+				{ 0.5, .9999 },
+				{ 0.5, .0001 },
+				{ 0.9999, 0.5 },
+				{ 0.9999, 0.5 },
+				{ 0.0001, .9999 },
+				{ 0.0001, .0001 },
+				{ 0.9999, .0001 },
+				{ 0.9999, .9999 },
+				{ 0.5, .9999 },
+				{ 0.5, .0001 },
+				{ 0.9999, 0.5 },
+				{ 0.9999, 0.5 },
+				{ 0.0001, .9999 },
+				{ 0.0001, .0001 },
+				{ 0.9999, .0001 },
+				{ 0.9999, .9999 },
+				{ 0.5, .9999 },
+				{ 0.5, .0001 },
+				{ 0.9999, 0.5 },
+				{ 0.9999, 0.5 },
+		};
+		boolean[] areZombies = new boolean[positions.length];
+		double[][] copyPositions = copy2DArray(positions);
+		// randomly set true/false, zombie/nonzombie assignment shouldn't matter
+		for (int i = 0; i < areZombies.length; i++) {
+			areZombies[i] = Math.random() < 0.5;
+		}
+		ZombieSimulator.updateEntities(areZombies, positions);
+		for (int i = 0; i < areZombies.length; i++) {
+			double dx = Math.abs(copyPositions[i][0]-positions[i][0]);
+			double dy = Math.abs(copyPositions[i][1]-positions[i][1]);
+			// assert true
+			boolean xValid = ZombieSimulator.RANDOM_DELTA_HALF_RANGE >= dx;
+			boolean yValid = ZombieSimulator.RANDOM_DELTA_HALF_RANGE >= dy;
+			// fail with descriptive message if distance is too far in either coordinate
+			assertTrue("At least one of your entities moved by more than RANDOM_DELTA_HALF_RANGE - check your random generation and boundary conditions.", xValid && yValid);
+		}
+
+	}
+	
+	/** 
+	 * Verify that different randoms are used for the x and y displacements.
+	 */
+	@Test
+	public void testDistinctXAndYMovement() {
+		int n = 100;
+		double[][] positions = new double[n][2];
+		boolean[] areZombies = new boolean[n];
+		for (int i = 0; i < positions.length; i++) {
+			// generates positions with padding within the boundary, so that boundary conditions do not affect test results
+			positions[i][0] = Math.random()*(1-2*ZombieSimulator.RANDOM_DELTA_HALF_RANGE) + ZombieSimulator.RANDOM_DELTA_HALF_RANGE;
+			positions[i][1] = Math.random()*(1-2*ZombieSimulator.RANDOM_DELTA_HALF_RANGE) + ZombieSimulator.RANDOM_DELTA_HALF_RANGE;
+			areZombies[i] = Math.random() < 0.5;
+		}
+		double[][] copyPosition = copy2DArray(positions);
+		ZombieSimulator.updateEntities(areZombies, positions);
+		 // fail if everything has the same x/y random displacement
+		int identicalDisplacements = 0;
+		for (int i = 0; i < n; i++) {
+			// if the displacement is nearly identical on the two coordinates
+			if (Math.abs(Math.abs(positions[i][0]-copyPosition[i][0])-Math.abs(positions[i][1]-copyPosition[i][1])) < 0.0000001){
+				identicalDisplacements++;
+			}
+		}
+		// fail with descriptive message
+		assertTrue("Too many entities moved by the same amount in their X and Y coordinates - make sure to use different randoms for each.", identicalDisplacements != n);
+	}
+	
+	/**
+	 * Verify that randoms generated can be both positive and negative 
+	 * (functionality covered by the testSingleZombieMotion, but this fails with a message)
+	 */
+	@Test
+	public void testRandomRange() {
+		int n = 100;
+		double[][] positions = new double[n][2];
+		boolean[] areZombies = new boolean[n];
+		for (int i = 0; i < positions.length; i++) {
+			positions[i][0] = Math.random();
+			positions[i][1] = Math.random();
+			areZombies[i] = Math.random() < 0.5;
+		}
+		double[][] copyPosition = copy2DArray(positions);
+		ZombieSimulator.updateEntities(areZombies, positions);
+		int positiveDisplacements = 0; // if all are positive, fail
+		for (int i = 0; i < n; i++) {
+			if (positions[i][0]-copyPosition[i][0] >= 0 && positions[i][1]-copyPosition[i][1] >= 0) {
+				positiveDisplacements++;
+			}
+		}
+		assertTrue("All of your randomly generated movement was positive - make sure the range is (-delta, delta).", positiveDisplacements != n);
+
+	}
+	
+	/**
+	 * Verify that each entity gets their own random displacements, rather than generating a single random X 
+	 * and random Y at the start and applying them to each entity.
+	 */
+	@Test
+	public void testUniqueEntityMovement() {
+		int n = 50;
+		double[][] positions = new double[n][2];
+		boolean[] areZombies = new boolean[n];
+		for (int i = 0; i < positions.length; i++) {
+			// generates positions with padding within the boundary, so that boundary conditions do not affect test results
+			positions[i][0] = Math.random()*(1-2*ZombieSimulator.RANDOM_DELTA_HALF_RANGE) + ZombieSimulator.RANDOM_DELTA_HALF_RANGE;
+			positions[i][1] = Math.random()*(1-2*ZombieSimulator.RANDOM_DELTA_HALF_RANGE) + ZombieSimulator.RANDOM_DELTA_HALF_RANGE;
+			areZombies[i] = Math.random() < 0.5;
+		}
+		double[][] copyPosition = copy2DArray(positions);
+		ZombieSimulator.updateEntities(areZombies, positions);
+		double[][] displacements = new double[n][2];
+		// calculate deltas for each coordinate
+		for (int i = 0; i < n; i++) {
+			displacements[i][0] = positions[i][0] - copyPosition[i][0];
+			displacements[i][1] = positions[i][1] - copyPosition[i][1];
+		}
+		// fail if all x displacements are equal and all y displacements are equal
+		int sameX = 0;
+		int sameY = 0;
+		for (int i = 1; i < n; i++) {
+			if (Math.abs(displacements[i][0]- displacements[i-1][0]) < 0.000001) {
+				sameX++;
+			}
+			if (Math.abs(displacements[i][1]- displacements[i-1][1]) < 0.000001) {
+				sameY++;
+			}
+		}
+		// descriptive error message
+		assertTrue("Each entity is moving by the same amount - make sure to re-generate random displacements for each entity.", (sameX != n-1) && (sameY != n-1));
+	}
+	
 }
